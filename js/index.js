92073503a85679cbbded3e11780df73094f8b63a
Jair0305 <chamaco_03@hotmail.es>
2023-03-28 10:23:41 -0600

    proyecto casi terminado

--- js/index.js
+++ js/index.js
@@ -1,28 +1,101 @@
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/json',
-		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-		'X-RapidAPI-Host': 'sudoku-solver3.p.rapidapi.com'
-	},
-	body: '{"input":[0,0,8,9,0,0,4,0,0,0,0,5,6,0,0,0,0,0,3,0,0,7,0,0,6,0,9,5,0,0,0,0,4,0,2,0,0,0,0,0,0,0,0,6,5,0,0,2,0,0,1,3,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,8,9,0,0,0,7,0,4,0,0,2,0,0]}'
-};
-
-fetch('https://sudoku-solver3.p.rapidapi.com/sudokusolver/', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
-    });
-
-    // Or with jQuery
-
-    $(document).ready(function(){
-    $('.parallax').parallax();
+	var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+});
+
+let lenguajes;
+let originValue, destinyValue 
+
+document.addEventListener('DOMContentLoaded', () => {
+	let originLanguage = document.querySelector('.origin-language');
+	let destinyLanguage = document.querySelector('.destiny-language')
+	const instance = M.FormSelect.init(originLanguage);
+	const instanceRight = M.FormSelect.init(destinyLanguage);
+
+	const options = {
+		method: 'GET',
+		headers: {
+			'Accept-Encoding': 'application/gzip',
+			'X-RapidAPI-Key': '2db93253c1msh578fc661660b48ap14541fjsn76b5571aa65b',
+			'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+		}
+	};
+	
+	fetch('https://google-translate1.p.rapidapi.com/language/translate/v2/languages?target=es', options)
+	.then(response => response.json())
+	.then((response) => {
+		lenguajes = response.data.languages;
+		console.log(lenguajes);
+
+		for (let i = 0; i < lenguajes.length; i++) {
+			let lenguaje = lenguajes[i];
+
+			// Crear una nueva opción para el select de origen
+			const optionOrigin = document.createElement('option');
+			optionOrigin.value = lenguaje.language;
+			optionOrigin.text = lenguaje.name;
+			originLanguage.add(optionOrigin);
+
+			// Crear una nueva opción para el select de destino
+			const optionDestiny = document.createElement('option');
+			optionDestiny.value = lenguaje.language;
+			optionDestiny.text = lenguaje.name;
+			destinyLanguage.add(optionDestiny);
+		}
+
+		instance.destroy();
+		instanceRight.destroy();
+		M.FormSelect.init(originLanguage);
+		M.FormSelect.init(destinyLanguage);
+
+		let button = document.querySelector('#translate-button');
+
+
+		button.addEventListener('click', () =>  {
+			let textArea = document.querySelector('#textarea1')
+			let textAreaValue = textArea.value
+
+			originValue = originLanguage.value;
+			destinyValue = destinyLanguage.value;
+
+
+			const encodedParams = new URLSearchParams();
+			encodedParams.append("q", `${textAreaValue}`);
+			encodedParams.append("target", `${destinyValue}`);
+			encodedParams.append("source", `${originValue}`);
+
+
+			const options = {
+				method: 'POST',
+				headers: {
+					'content-type': 'application/x-www-form-urlencoded',
+					'Accept-Encoding': 'application/gzip',
+					'X-RapidAPI-Key': '2db93253c1msh578fc661660b48ap14541fjsn76b5571aa65b',
+					'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+				},
+				body: encodedParams
+			};
+
+			fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
+				.then(response => response.json())
+				.then(response => {
+					const traductionParagraph = document.querySelector('.response')
+					console.log(response)
+					const translation = response.data.translations[0].translatedText
+					traductionParagraph.innerHTML = `${translation}`
+				})
+				.catch(err => console.error(err));
+		})
+
+	})
+	.catch(err => console.error(err));
+
+});
+
